9246379fdbf3cd3cf70900d6114ad33066427abc
Rohith09 <kandirohith09@gmail.com>
2018-05-25 16:28:55 +0530

    Upcasting in Java

---
+++ Upcasting.java
@@ -0,0 +1,9 @@
+ class upcast
+{
+	public static void main(String[]args)
+	{
+		byte a=22;    //The variable a is decleared as BYTE and a value of 22 has been assigned to it.
+		int b=a;       // Here we are performing the type casting by changing the data type of the variable a.
+		System.out.println("The value of b is = "+b);
+	}
+}
