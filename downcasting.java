89d5831b4b4a9a3206d090dc8ebebbbd2dfc2b25
Rohith09 <kandirohith09@gmail.com>
2018-05-25 16:35:24 +0530

    Downcasting in Java

---
+++ downcasting.java
@@ -0,0 +1,9 @@
+class downcasting
+{
+	public static void main(String []args)
+	{
+		double  a=10;
+		int b = (int)a;  // Explicitly cast one data type to another using the "()" cast operator.
+		System.out.println("The value of b is "+a);
+	}
+}
