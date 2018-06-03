ed6cadcb8b3b329bbf9c72c7ba82d4b35fcf3176
Rohith09 <kandirohith09@gmail.com>
2018-06-03 21:58:27 +0530

    More on  1D array in

--- 1DArray/src/pkg1darray/Main.java
+++ 1DArray/src/pkg1darray/Main.java
@@ -5,17 +5,30 @@ public class Main {
     public static void main(String[] args) {
         int a[] = new int []{4,5,6,7,8,9};
         ArrayOp.print(a);
+        ArrayOp ao = new ArrayOp();
+        System.out.println("The sum of the elements in the array is "+ao.add(a));
         
     }
     
 }
 class ArrayOp
 {
-    static void print(int a[])
+    static void print(int a[])  //Method for printing all the values present within the array
     {
         for(int x:a)
         {
             System.out.println(x);
         }
     }
+       int add(int a[])        //Method for calculating the sum of all the elements in the array.
+    {
+        int s=0;
+        for(int x:a)
+        {
+           s+=x; 
+        }
+        return s;
+
+    }
 }
+ 
