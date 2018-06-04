31d9ac9228b2962b4fdeea5fbc20b77857474b40
Rohith09 <kandirohith09@gmail.com>
2018-06-04 11:38:25 +0530

    More on 2D Array

--- 2DArray/src/pkg2darray/Main.java
+++ 2DArray/src/pkg2darray/Main.java
@@ -1,12 +1,18 @@
 
 package pkg2darray;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        int a[][]= new int [][]{{1,2,3},{4,5,6},{7,8,9}};
+        int a[][]= new int [][]{{1,2,3},{4,5,6},{6,6,9}};
         ArrayOp.print(a);
         ArrayOp ao = new ArrayOp();
         System.out.println("Sum= "+ao.add(a));
+        ao.max(a);
+        ao.min(a);
+        ao.freq(a);
         
     }
     
@@ -37,4 +43,67 @@ class ArrayOp
         }
        return s;
     }
+    void max(int a[][])       //To find the maximum element in the array
+            {
+                for(int x[]:a)
+                {
+                    int max = x[0];
+                    for(int y:x)
+                       {
+                        if(y>max)
+                            {
+                                 max=y;
+                              
+                            }
+                         
+
+                        }          
+                    System.out.println("The max element is "+max);
+
+                }
+
+            }
+    void min(int a[][])     //To find the minimum element in the array
+            {
+                for(int x[]:a)
+                {
+                    int min = x[0];
+                    for(int y:x)
+                       {
+                        if(y<min)
+                            {
+                                 min=y;
+                              
+                            }
+                         
+
+                        }          
+                    System.out.println("The min element is "+min);
+
+                }
+
+            }
+    void freq(int a[][])       //To find the frequency of the elements
+    {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int z=0;
+        for(int x[]:a)
+        {
+            for(int y:x)
+            {
+                if(map.containsKey(y))
+                {
+                    map.put(y, z+1);
+                }
+                else
+                {
+                    map.put(y, 1);
+                }
+            }
+        }
+        System.out.println(map);
+    }
 }
+
+
+    
