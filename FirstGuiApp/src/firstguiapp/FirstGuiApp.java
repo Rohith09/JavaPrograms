13e028c52bb2d9225fff42158c997dab2697384e
Rohith09 <kandirohith09@gmail.com>
2018-06-06 20:06:38 +0530

    Effective mwthod of Creating a GUI Application

--- FirstGuiApp/src/firstguiapp/FirstGuiApp.java
+++ FirstGuiApp/src/firstguiapp/FirstGuiApp.java
@@ -2,23 +2,32 @@
 package firstguiapp;
 
 import java.awt.*;
+class Gui extends Frame
+{
 
+    Gui()
+    {
+        Frame f = new Frame();
+        f.setVisible(true);
+        f.setLayout(null);
+        f.setSize(400, 400);
+        f.setTitle("First GUI Application");
+        Button b = new Button("OK");
+        b.setBounds(50, 50, 50, 50);
+        f.add(b);
+        Label l = new Label("Hello World");
+        l.setBounds(50, 100,80,80);
+        f.add(l);
+        TextField t = new TextField();
+        t.setBounds(50, 150, 80, 80);
+        f.add(t);
+
+    }
+}
 public class FirstGuiApp {
 public static void main(String[] args) {
-    Frame f = new Frame();
-    f.setVisible(true);
-    f.setSize(400, 400);
-    f.setLayout(null);
-    f.setTitle("My First Gui Application");
-    Button b = new Button("OK");
-    b.setBounds(50, 50, 50, 50);
-    f.add(b);
-    Label l = new Label("Hello World");
-    l.setBounds(50, 100,80,80);
-    f.add(l);
-    TextField t = new TextField();
-    t.setBounds(50, 150, 80, 80);
-    f.add(t);
+
+    Gui g = new Gui();
 }
     
 }
