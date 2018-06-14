93337f839a38df301c1a374853edea21a882d1af
Rohith09 <kandirohith09@gmail.com>
2018-06-15 00:53:59 +0530

    Patterns in java

---
+++ Patterns/pattern3.java
@@ -0,0 +1,30 @@
+class A
+{
+	void print()
+	{
+		for (int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				if(i==1||i==5||j==1||j==5)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
+class pattern3
+{
+	public static void main(String args[])
+	{
+			A a = new A();
+	 		a.print();
+	}
+
+}
