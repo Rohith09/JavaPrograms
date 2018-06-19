1abeca5b80ec1bdd8f329661359bfc44e09be715
Rohith09 <kandirohith09@gmail.com>
2018-06-19 22:14:07 +0530

    Patterns in java

---
+++ Patterns/pattern11.java
@@ -0,0 +1,35 @@
+class A
+{
+	public void print()
+	{
+		int n=5;
+		int z=1;
+		for(int i=1;i<=n;i++)
+		{
+			for (int j=n-1;j>=i;j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=1;k<=z;k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+			z++;
+			
+
+		}
+
+	}
+
+}
+class pattern11
+{
+	public static void main(String []args)
+	{
+		A a = new A();
+		a.print();
+
+	}
+}
+
