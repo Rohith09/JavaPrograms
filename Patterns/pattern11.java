06c741424f25e44475b44af6ccda77d89a1aae34
Rohith09 <kandirohith09@gmail.com>
2018-06-19 22:20:06 +0530

    Patterns in java

--- Patterns/pattern11.java
+++ Patterns/pattern11.java
@@ -3,19 +3,19 @@ class A
 	public void print()
 	{
 		int n=5;
-		int z=1;
+		//int z=1;
 		for(int i=1;i<=n;i++)
 		{
 			for (int j=n-1;j>=i;j--)
 			{
 				System.out.print(" ");
 			}
-			for(int k=1;k<=z;k++)
+			for(int k=1;k<=i;k++)
 			{
 				System.out.print("*");
 			}
 			System.out.println();
-			z++;
+			//z++;
 			
 
 		}
