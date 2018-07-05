fcdccb13955559796a23c539cff65c9de8b241b1
Rohith09 <kandirohith09@gmail.com>
2018-07-05 20:40:57 +0530

    Program on Big Integer in java

---
+++ BigInteger.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Scanner;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       BigInteger a= new BigInteger(in.next());
+       BigInteger b= new BigInteger(in.next());
+       BigInteger sum = a.add(b);
+       BigInteger pro = a.multiply(b);
+       System.out.println(sum);
+       System.out.println(pro);
+        
+        
+
+        
+        
+        
+    }
+}
