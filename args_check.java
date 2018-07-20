f1839de091bead2cec206b59915bbc15d333f445
Rohith Kandi <kandirohith09@gmail.com>
2018-07-20 07:35:02 +0530

    Java Fundamentals (Wipro Training)

---
+++ args_check.java
@@ -0,0 +1,17 @@
+class args_check
+{
+	public static void main(String []args)
+	{
+		if(args.length==0)
+		{
+			System.out.println("No Values");
+		}
+		else
+		{
+			for(int i=0;i<=args.length-1;i++)
+			{
+				System.out.print(args[i]+" ");
+			}
+		}
+	}
+}
