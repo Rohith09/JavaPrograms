af58942ddb7443399c394e13391e2a6b4fcfb6e4
Rohith Kandi <kandirohith09@gmail.com>
2018-07-27 11:53:54 +0530

    Code to create a new java file in D drive.

---
+++ CreateFile.java
@@ -0,0 +1,49 @@
+import java.io.File;
+
+ class CreateFile
+{
+
+    
+ public static void main(String []args)
+   
+  {
+ 
+      try
+     
+      {
+   
+         File file = new File("D:\\hello.txt");
+   
+         boolean stat =file.createNewFile();
+ 
+            if(stat)
+    
+               {
+           
+                  System.out.println("File Successfully created!");
+    
+               }
+  
+            else
+    
+               {
+            
+                  System.out.println("File not created!");
+       
+               }
+     
+   
+    }
+      
+       catch(Exception ex)
+     
+       {
+         
+           System.out.println(ex);
+    
+       }
+        
+ 
+   }
+
+}
